685eb55b58721256d957735e866205d492e7aafb
msierr37 <msierr37@eafit.edu.co>
2018-05-21 16:07:51 -0500

    Organizando errores

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,8 +1,8 @@
 class ReportMailer < ApplicationMailer
     default from: "tucultivo1997@gmail.com"
     
-    def sample_email(user)
-        @user = user
-        mail(to: user, subject: 'Sample Email')
-    end
+    #def sample_email(user)
+     #   @user = user
+      #  mail(to: 'msierr37@eafit.edu.co', subject: 'Sample Email')
+    #end
 end
